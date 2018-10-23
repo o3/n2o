1e26beddc7d1b58cc28834ac72a0e4ca1535188e
Marat Khafizov <mhafizov@i-novus.ru>
2018-10-23 08:38:08 +0300

    Fix tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,27 +5,27 @@ import Data.BERT
 
 main :: IO ()
 main = do
-  (t1, t2, _) <- protoRun NilTerm cx
+  (t1, t2, _) <- protoRun [] cx
   hspec $ do
     describe "nop reply test" $ do
       it "test1" $ do
-        t1 `shouldBe` reply
+        t1 `shouldBe` "reply"
       it "test2" $ do
-        t2 `shouldBe` (TupleTerm [binary, NilTerm])
-  let proto1 = AtomTerm "proto1"
+        t2 `shouldBe` ["binary", []]
+  let proto1 = "proto1"
   (t1, t2, _) <- protoRun proto1 cx
   hspec $ do
     describe "proto1 reply test" $ do
       it "test1" $ do
-        t1 `shouldBe` reply
+        t1 `shouldBe` "reply"
       it "test2" $ do
         t2 `shouldBe` proto1
-  let proto2 = AtomTerm "proto2"
+  let proto2 = "proto2"
   (t1, t2, _) <- protoRun proto2 cx
   hspec $ do
     describe "proto2 reply test" $ do
       it "test1" $ do
-        t1 `shouldBe` reply
+        t1 `shouldBe` "reply"
       it "test2" $ do
         t2 `shouldBe` proto2
 
@@ -34,15 +34,15 @@ cx = defaultCx{cxProtos = protos}
 proto1 = Proto
   { protoInfo = \msg state ->
       case msg of
-        p1@(AtomTerm "proto1") -> return (reply, p1, state)
-        _ -> return (unknown, msg, state)
+        "proto1" -> return ("reply", "proto1", state)
+        _ -> return ("unknown", msg, state)
   , protoInit = return ()
   }
 proto2 = Proto
   { protoInfo = \msg state ->
      case msg of
-       p2@(AtomTerm "proto2") -> return (reply, p2, state)
-       _ -> return (unknown, msg, state)
+       "proto2" -> return ("reply", "proto2", state)
+       _ -> return ("unknown", msg, state)
   , protoInit = return ()
   }
 
